a53343a2e506001a801727c7082320cc6cf57361
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-05-28 10:21:32 +0200

    Implemented process.NameOf(pid) for Linux using /proc virtual filesystem

---
+++ procfsutil.go
@@ -0,0 +1,31 @@
+// +build !darwin linux
+
+package process
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const procFsPidPath = "/proc/%d/%s"
+
+func openPid(pid int, name string) (*os.File, error) {
+	return os.Open(fmt.Sprintf(procFsPidPath, pid, name))
+}
+
+func parseProcName(status *os.File) string {
+	procName := ""
+
+	scanner := bufio.NewScanner(status)
+	if scanner.Scan() {
+		if parts := strings.Split(scanner.Text(), ":"); len(parts) == 2 {
+			procName = strings.TrimSpace(parts[1])
+		}
+	}
+	//if err := scanner.Err(); err != nil {
+	//}
+
+	return procName
+}
