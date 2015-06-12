e8289dea42409c61af6f43d55285886c08de8ca4
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-12 23:11:24 +0200

    Organizing internal code

--- process/internal/procfsutil.go
+++ process/internal/procfsutil.go
@@ -2,7 +2,7 @@
 
 // +build !darwin linux
 
-package process
+package internal
 
 import (
 	"bufio"
--- process/internal/util_linux.go
+++ process/internal/util_linux.go
@@ -1,6 +1,6 @@
 // Copyright 2015 Giacomo Stelluti Scala. All rights reserved. See doc/License.md in the project root for license information.
 
-package process
+package internal
 
 import "strconv"
 
